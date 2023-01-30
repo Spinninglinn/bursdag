95ad1591ca104e9b908c922bbf5749730c948738
LinnAnette <linnanette.o@gmail.com>
2023-01-30 20:44:43 +0100

    flex og mer styr

--- js/btgg.js
+++
@@ -1,7 +0,0 @@
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("ip").innerHTML = "IP-adresse: " + data.ip;
-  });
-
-  console.log;
